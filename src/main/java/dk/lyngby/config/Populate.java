3731a932cf47797fac694b804b140c7206a932e0
Chris Hedin Ziegler <60019587+ChriZZZy@users.noreply.github.com>
2023-12-20 11:46:02 +0100

    fixed drop table script in populate

--- src/main/java/dk/lyngby/config/Populate.java
+++ src/main/java/dk/lyngby/config/Populate.java
@@ -9,14 +9,23 @@ import jakarta.persistence.EntityManagerFactory;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Populate {
+
+
     public static void main(String[] args) {
 
+        dropTables(); // Call method to drop tables
+
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+
 
         try (var em = emf.createEntityManager()) {
             em.getTransaction().begin();
@@ -28,7 +37,7 @@ public class Populate {
             Role roleUser = new Role("ROLE_USER");
             Role roleAdmin = new Role("ROLE_ADMIN");
 
-            Diary diaryToUser1 = new Diary( "About Stefan", 150);
+            Diary diaryToUser1 = new Diary("About Stefan", 150);
             Diary diaryToUser2 = new Diary("School Time", 150);
 
             em.persist(user1);
@@ -76,4 +85,27 @@ public class Populate {
         return pages;
     }
 
+    private static void dropTables() {
+        String url = "jdbc:postgresql://localhost:5432/diary";
+        String user = "postgres";
+        String password = "postgres";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            Statement statement = connection.createStatement();
+
+            // Drop tables
+            statement.executeUpdate("DROP TABLE IF EXISTS user_roles");
+            statement.executeUpdate("DROP TABLE IF EXISTS users");
+            statement.executeUpdate("DROP TABLE IF EXISTS roles");
+            statement.executeUpdate("DROP TABLE IF EXISTS diarypage");
+            statement.executeUpdate("DROP TABLE IF EXISTS diary");
+
+            System.out.println("Tables dropped successfully!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
+
